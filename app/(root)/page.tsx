5f2f2a7ac458b4395b27cbf4113ef72ebf6abe4c
Lucas Dias <91148856+LucaFDias@users.noreply.github.com>
2023-06-29 15:57:12 -0300

    feat: first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  
+  return (
+    <div className="p-4">
+      <Button size="default">Clique aqui</Button>
+
+    </div>
+  )
+}
